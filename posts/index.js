352a46f07da81de8b9d09a12a088e19a2f64cab9
Mali McCalla <malimccalla@gmail.com>
2020-05-10 11:03:54 +0100

    Add k8s config

--- posts/index.js
+++ posts/index.js
@@ -20,15 +20,15 @@ app.post('/posts', async (req, res) => {
 
   posts[id] = {
     id,
-    title
+    title,
   };
 
   await axios.post('http://localhost:4005/events', {
     type: 'PostCreated',
     data: {
       id,
-      title
-    }
+      title,
+    },
   });
 
   res.status(201).send(posts[id]);
@@ -41,5 +41,7 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log('version 55');
+
   console.log('Listening on 4000');
 });
