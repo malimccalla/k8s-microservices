241eb17ebc8edd2749e4c0a6bb056b43d8b8293e
Mali McCalla <malimccalla@gmail.com>
2020-05-11 08:43:28 +0100

    Update post create handpoint

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ export default () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post('http://dev.k8s.com/posts', {
+    await axios.post('http://dev.k8s.com/posts/create', {
       title,
     });
 
