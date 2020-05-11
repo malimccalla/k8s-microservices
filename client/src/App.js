e08dc24c54143e81e7e15c75a2fd230e8f892118
Mali McCalla <malimccalla@gmail.com>
2020-05-11 09:21:53 +0100

    Add skaffold

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import PostCreate from './PostCreate';
 import PostList from './PostList';
 
 export default () => {
   return (
     <div className="container">
-      <h1>Create Post</h1>
+      <h1>Create Post.</h1>
       <PostCreate />
       <hr />
       <h1>Posts</h1>
